0153ef1e92444697b351afea395524e91f533f9d
cubl1 <liptonromurat@gmail.com>
2024-12-30 10:11:05 +0300

    Add files via upload

---
+++ jtoh.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+user TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER
+)
+''')
+
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (user, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i}', f'example{i}.gmail.com', i*10, 1000))
+    if i % 2 == 1:
+        cursor.execute('UPDATE Users SET balance = ? WHERE user = ?', (500, f'User{i}'))
+    if i % 3 == 1:
+        cursor.execute('DELETE FROM Users WHERE user = ?', (f'User{i}',))
+
+cursor.execute('SELECT * FROM Users WHERE age != 60')
+hi = cursor.fetchall()
+for i in hi:
+    print(f'Имя: {i[1]} | Почта: {i[2]} | Возраст: {i[3]} | Баланс: {i[4]}')
+
+cursor.execute('DELETE FROM Users WHERE age = ?', (60,))
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+total_b = cursor.fetchone()[0]
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_u = cursor.fetchone()[0]
+print(total_b / total_u)
+
+connection.commit()
+connection.close()
